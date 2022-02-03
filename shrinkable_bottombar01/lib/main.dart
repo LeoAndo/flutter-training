722776c96b58e8eab2db424fc91ab744e81c2c4c
LeoAndo <leo.777.info@gmail.com>
2022-02-03 17:08:58 +0900

    スクロールで縮むフッター #44

--- shrinkable_bottombar01/lib/main.dart
+++ shrinkable_bottombar01/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'bottom_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,8 +40,98 @@ class ShrinkableFooterPage extends StatefulWidget {
 }
 
 class _ShrinkableFooterPageState extends State<ShrinkableFooterPage> {
+  final _scrollController = ScrollController();
+  bool isHiding = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _listenScroll();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(() {});
+    super.dispose();
+  }
+
+  void _listenScroll() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        isHiding = false;
+      } else {
+        isHiding = true;
+      }
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ListView(
+              controller: _scrollController,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 16.0, right: 50),
+                  height: 200,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(20.0),
+                        bottomLeft: Radius.circular(20.0)),
+                    color: Color(0xFFFEEAE6),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      SizedBox(height: 70),
+                      Text(
+                        'スクロールに応じて\nBottomNavigationBarが\n縮みます。',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24.0,
+                          color: Color(0xFF442C2E),
+                          height: 1.2,
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Image.network(
+                    'https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
+                const SizedBox(height: 20.0),
+                Image.network(
+                    'https://images.unsplash.com/photo-1532236204992-f5e85c024202?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1372&q=80'),
+                const SizedBox(height: 20.0),
+                Image.network(
+                    'https://images.unsplash.com/photo-1493479874819-4303c36fa0f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'),
+                const SizedBox(height: 20.0),
+                Image.network(
+                    'https://images.unsplash.com/photo-1463319611694-4bf9eb5a6e72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'),
+                const SizedBox(height: 20.0),
+                Image.network(
+                    'https://images.unsplash.com/photo-1469998265221-245c7148df5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'),
+                const SizedBox(height: 20.0),
+              ],
+            ),
+            Positioned(
+              bottom: 0,
+              child: MyBottomNavigationBar(
+                isHiding: isHiding,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
