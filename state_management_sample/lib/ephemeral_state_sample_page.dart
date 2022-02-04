63148b9099f945737bc4a1b7889b66f4f76b8baf
LeoAndo <leo.777.info@gmail.com>
2022-02-04 18:36:03 +0900

    #3 Ephemeral stateのサンプル

--- state_management_sample/lib/ephemeral_state_sample_page.dart
+++ state_management_sample/lib/ephemeral_state_sample_page.dart
@@ -8,11 +8,35 @@ class EphemeralStatePage extends StatefulWidget {
 }
 
 class _EphemeralStatePageState extends State<EphemeralStatePage> {
+  int _navBarIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Ephemeral State Sample"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.food_bank),
+            label: 'FoodBank',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'Alarm',
+          ),
+        ],
+        currentIndex: _navBarIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: (newIndex) {
+          setState(() {
+            _navBarIndex = newIndex;
+          });
+        },
       ),
     );
   }
